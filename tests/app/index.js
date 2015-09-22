c516ae20d9e81583a69b1455ea8cb208baaa93b8
Jonathan Grimes <jsg2021@gmail.com>
2015-09-22 14:03:14 -0500

    update test app to show output.

--- tests/app/index.js
+++ tests/app/index.js
@@ -19,7 +19,36 @@ React.render(
 	document.getElementById('content')
 );
 
+const Frame = React.createClass({
+	displayName: 'Frame',
+
+	onChange () {
+		const {refs: {editor}} = this;
+		let value = editor.getValue();
+		//The value will be an array of parts. Parts will be based on what
+		//onPartValueParseCallback returns. By default it should be all html
+		//fragments. The value will split on Block Elements.
+		this.setState({value});
+	},
+
+	render () {
+		let {value} = this.state || {};
+
+		return (
+			<div>
+				<Editor ref="editor" onChange={this.onChange}/>
+				<div>Value Raw:</div>
+				<textarea style={{width: '100%', height: 100}} value={value}/>
+				<div>
+					Value (in dom):<hr/>
+					<div dangerouslySetInnerHTML={{__html: value}}/>
+				</div>
+			</div>
+		);
+	}
+});
+
 React.render(
-	React.createElement(Editor),
+	React.createElement(Frame),
 	document.getElementById('content2')
 );
